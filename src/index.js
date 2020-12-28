6b4bb295bcd9ce82e33ad86441db2e7aa0b96750
Emilia <emilia.lach.91@gmail.com>
2020-12-28 15:46:20 +0000

    section arrows added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles/main.scss';
 // import './style/reset/reset.css';
 
 const button = document.querySelector('.hero__invitation');
-const element = document.querySelector('.project');
+const element = document.querySelector('.project__surf');
 
 button.addEventListener('click', function () {
     element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
