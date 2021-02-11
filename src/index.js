2d45c9aa06b82c5097dc9eefaeaa47711503d554
Emilia <emilia.lach.91@gmail.com>
2021-02-11 16:04:16 +0000

    organizer app add

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles/main.scss';
 // import './style/reset/reset.css';
 
 const button = document.querySelector('.hero__invitation');
-const element = document.querySelector('.project__surf');
+const element = document.querySelector('.project__organizer');
 
 button.addEventListener('click', function () {
     element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
