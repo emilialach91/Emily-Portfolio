a52f5865f3bd91e42cffd29cfc7f595bc25d1845
Emilia <emilia.lach.91@gmail.com>
2021-02-17 11:36:59 +0000

    covid app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles/main.scss';
 
 
 const button = document.querySelector('.hero__invitation');
-const element = document.querySelector('.project__organizer');
+const element = document.querySelector('.project__covid19');
 
 button.addEventListener('click', function () {
     element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
